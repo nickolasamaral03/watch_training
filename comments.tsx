902f1cedaaeebdd89a38b144cf487b1954b3d422
Nickolas Amaral <125821680+nickolasamaral03@users.noreply.github.com>
2023-12-30 17:50:33 -0300

    subindo projeto

---
+++ comments.tsx
@@ -0,0 +1,12 @@
+// Documentar tudo depois:
+
+// melhorar o design se quiser;
+
+// Deixa-lo responisvo;
+
+
+// Melhorias:
+// Deixar o relógio vibrando conforme o cronometro esteja contando;
+// Opção de excluír o treino;
+// De parar o tempo no relógio (com um botão de preferência);
+// Ao atualizar não perca os treinos "salvos" utilizando recursos do próprio navegador;
